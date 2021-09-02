8bba7856c6e539fb25aef3eacc386b7f3619ac0c
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-09-02 10:23:38 -0300

    removing unused imports

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -2,14 +2,10 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import Icon from '@material-ui/core/Icon';
 import { Typography } from '@material-ui/core';
 
